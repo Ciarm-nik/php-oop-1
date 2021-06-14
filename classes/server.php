13b25a9a3adde48edfad14bbc1edb54e1102e53b
Ciarm-nik <nikcia@libero.it>
2021-06-14 17:30:37 +0200

    Aggiunto un metodo e stampato le relative proprietà

--- classes/server.php
+++ classes/server.php
@@ -1,22 +1,39 @@
-<!-- - è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 
-<?php 
 
-class Movie {
-    public $titolo;
-    public $regia;
-    public $anno;
+ <?php
 
-    function __construct($titolo, $regia, $anno ){
-        $this ->titolo = $titolo;
-        $this ->regia = $regia;
-        $this ->anno = $anno;        
+
+    // è definita una classe ‘Movie’
+    class Movie
+    {
+        // => all'interno della classe sono dichiarate delle variabili d'istanza
+        public $titolo;
+        public $regia;
+        public $anno;
+
+
+        // => all'interno della classe è definito un costruttore
+        function __construct($titolo, $regia, $anno)
+        {
+            $this->titolo = $titolo;
+            $this->regia = $regia;
+            $this->anno = $anno;
+        }
+
+
+        // => all'interno della classe è definito almeno un metodo
+        public function dettagliMovie()
+        {
+            return $this->titolo . " " . $this->regia;
+        }
+
+
+        // - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+        public function printMovie()
+        {
+            echo "<h1>" . $this->dettagliMovie() . "</h1>";
+            echo "<h2>" . $this->anno . "</h2>";
+        }
     }
-    
-}
 
-?>
+    ?>
--- index.php
+++ index.php
@@ -6,5 +6,13 @@ $django = new Movie("Django Unchained ", "Tarantino", "2012");
 
 
 
-var_dump($killBill);
-var_dump($django);
+// var_dump($killBill);
+// var_dump($django);
+
+// var_dump($killBill->dettagliMovie());
+// var_dump($django->dettagliMovie());
+
+$killBill->printMovie();
+$django->printMovie();
+
+
